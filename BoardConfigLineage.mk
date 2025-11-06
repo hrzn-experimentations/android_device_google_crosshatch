4bad20f944d0a54d8ceb09ed66c4e50c9e8d713c
hrzndelta <hrzndelta@proton.me>
2025-11-06 14:25:19 +0800

    b1c1: Enable reserved storage spaces only when not enabling WITH_MICROG

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -27,7 +27,7 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # Reserve space for gapps install
 -include vendor/lineage/config/BoardConfigReservedSize.mk
-ifneq ($(WITH_GMS),true)
+ifneq ($(WITH_MICROG),true)
 BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 1069547520
 endif
 
